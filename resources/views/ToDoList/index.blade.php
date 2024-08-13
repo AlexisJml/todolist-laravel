19312ba29b64f09cae6e60e1344ef02a6a397e3d
jumel <alexis.jumel1@gmail.com>
2024-08-13 13:48:17 +0200

    Add modify button + pop-up

--- resources/views/ToDoList/index.blade.php
+++ resources/views/ToDoList/index.blade.php
@@ -36,11 +36,58 @@
                     <td class="px-6 py-4">{{ $task->priorité }}</td>
                     <td class="px-6 py-4">{{ $task->validé }}</td>
                     <td class="px-6 py-4">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <!-- Bouton d'affichage de la pop-up de modification -->
+                        <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="flex justify-center items-center text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm p-2.5" type="button">
+                            <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(270)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.29185 3.294C3.58094 3.00403 4.0176 2.91959 4.39395 3.08088L10.6939 5.78088L18.3939 9.08088C18.5109 9.131 18.6172 9.20297 18.7071 9.29292L21.7071 12.2929C22.0977 12.6834 22.0977 13.3166 21.7071 13.7071C21.3166 14.0977 20.6834 14.0977 20.2929 13.7071L18.0063 11.4205L11.4839 18.0585L13.7089 20.2947C14.0985 20.6862 14.0969 21.3194 13.7054 21.7089C13.3139 22.0985 12.6807 22.0969 12.2911 21.7054L9.36877 18.7683C9.28036 18.6794 9.20944 18.5747 9.15971 18.4597L5.79793 10.6809L3.08209 4.39674C2.91965 4.02088 3.00276 3.58397 3.29185 3.294ZM10.406 16.302L16.2613 10.3428L10.5284 7.88587L7.90329 10.511L10.406 16.302ZM7.04978 8.53607L8.5485 7.03734L5.91414 5.90833L7.04978 8.53607Z" fill="#1D4ED8"></path></g></svg>
+                        </button>
+                        <!-- intérieur de la pop-up de modification -->
+                        <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-md max-h-full">
+                                <!-- Contenu -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <!-- Header -->
+                                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                            Modifier
+                                        </h3>
+                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <!-- Body -->
+                                    <form class="p-4 md:p-5">
+                                        <div class="grid gap-4 mb-4 grid-cols-2">
+                                            <div class="col-span-2">
+                                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
+                                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="{{ $task->titre }}" placeholder="Titre de la tâche" required="">
+                                            </div>
+                                            <div class="col-span-2 sm:col-span-1">
+                                                <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Priorité</label>
+                                                <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                    <option value="haute" {{ $task->priorité == 'haute' ? 'selected' : '' }}>haute</option>
+                                                    <option value="moyenne" {{ $task->priorité == 'moyenne' ? 'selected' : '' }}>moyenne</option>
+                                                    <option value="faible" {{ $task->priorité == 'faible' ? 'selected' : '' }}>faible</option>
+                                                </select>
+                                            </div>
+                                            <div class="col-span-2">
+                                                <label for="contenu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contenu</label>
+                                                <textarea id="contenu" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here">{{ $task->contenu }}</textarea>                    
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            Modifier la tâche
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div> 
                     </td>
                 </tr>
             @endforeach
         </tbody>
-    </table>
+    </table> 
 </div>
 @endsection
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titre')</title>
     @vite('resources/css/app.css')
+    <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 </head>
 <body class="bg-white dark:bg-gray-900">
     <div class="mx-8 content-center">
