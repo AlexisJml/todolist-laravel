9019d137782108e5efab53a7bb37ce1049925d22
jumel <alexis.jumel1@gmail.com>
2024-08-14 16:34:55 +0200

    Add function Add_Task controller

--- app/Http/Controllers/ToDoListController.php
+++ app/Http/Controllers/ToDoListController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ToDoList;
 
 class ToDoListController extends Controller
 {
     public function Get_Tasks () {
         return view('ToDoList.index', [
-            'tasks' => \App\Models\ToDoList::where('validé', false)->get(),
-            'completed' => \App\Models\ToDoList::where('validé', true)->get()
+            'tasks' => ToDoList::where('validé', false)->get(),
+            'completed' => ToDoList::where('validé', true)->get()
         ]);
     }
 
-    public function Add_Task () {
-        
+    public function Add_Task (Request $request) {
+        $validatedData = $request->validate([
+            'titre' => 'required|string|max:255',
+            'contenu' => 'required|string',
+            'priorité' => 'required|in:faible,moyenne,haute',
+        ]);
+
+        //Création d'une nouvelle tâche
+        $task = new ToDoList();
+        $task->titre = $validatedData['titre'];
+        $task->contenu = $validatedData['contenu'];
+        $task->priorité = $validatedData['priorité'];
+        $task->timestamps();
+        $task->save(); 
+
+        //Rafraichi la page
+        return redirect()->back();
     }
 }
