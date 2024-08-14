2290b03c2adeeb971469150fb19b067e17f4b97b
jumel <alexis.jumel1@gmail.com>
2024-08-14 10:50:11 +0200

    add modal validation

--- resources/views/ToDoList/index.blade.php
+++ resources/views/ToDoList/index.blade.php
@@ -78,10 +78,10 @@
                                             <p>Voulez-vous vraiment supprimer cette tâche?</p>
                                         </div>
                                         <div class="flex justify-center space-x-4">
-                                            <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            <button type="submit" class="text-blue-700 inline-flex items-center border border-blue-700 hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-600 dark:text-blue-600 dark:hover:bg-blue-700 dark:hover:text-white dark:focus:ring-blue-800">
                                                 Annuler
                                             </button>
-                                            <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            <button type="submit" class="text-white inline-flex items-center bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                                                 Supprimer
                                             </button>
                                         </div>
@@ -133,7 +133,43 @@
                                     </form>
                                 </div>
                             </div>
-                        </div> 
+                        </div>
+                        
+                        <!-- intérieur de la pop-up de validation -->
+                        <div id="{{ $modalValId }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-md max-h-full">
+                                <!-- Contenu -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <!-- Header -->
+                                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                            Valider
+                                        </h3>
+                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="{{ $modalValId }}">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <!-- Body -->
+                                    <form class="p-4 md:p-5">
+                                        <div class="grid gap-4 mb-4">
+                                            <p>Voulez-vous vraiment valider cette tâche?</p>
+                                        </div>
+                                        <div class="flex justify-center space-x-4">
+                                            <button type="submit" class="text-blue-700 inline-flex items-center border border-blue-700 hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-600 dark:text-blue-600 dark:hover:bg-blue-700 dark:hover:text-white dark:focus:ring-blue-800">
+                                                Annuler
+                                            </button>
+                                            <button type="submit" class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                                Valider
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
                     </td>
                 </tr>
             @endforeach
