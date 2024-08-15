1ab05c082af551c07e41f3ebffb5a4675b65a0dd
jumel <alexis.jumel1@gmail.com>
2024-08-15 16:35:08 +0200

    redirect home to todolist

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-    // return \route('todolist.index');
+    return redirect()->route('todolist.index');
 });
 
 
